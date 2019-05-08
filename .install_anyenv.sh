e1693cdb52f57f45bc0d26768b3084c87acd221a
Narazaka <info@narazaka.net>
2019-05-08 17:32:33 +0900

    anyenv

--- .install_anyenv.sh
+++ .install_anyenv.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 git clone https://github.com/riywo/anyenv.git .anyenv
 echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.zshenv
+~/.anyenv/bin/anyenv install --init
 echo 'eval "$(anyenv init -)"' >> ~/.zshenv
-exec $SHELL -l
-anyenv install plenv
-anyenv install rbenv
-anyenv install pyenv
-anyenv install ndenv
-exec $SHELL -l
+exec
