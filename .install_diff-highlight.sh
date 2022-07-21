8e37443d53c41e76b0cedfaf0ba9f2076ac20739
Narazaka <info@narazaka.net>
2022-07-21 09:06:35 +0900

    old

--- .install_diff-highlight.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git clone --depth 1 --single-branch https://github.com/git/git.git .git-master
-cd ~/.git-master/contrib/diff-highlight && make
-cd
-echo 'export PATH="$HOME/.git-master/contrib/diff-highlight:$PATH"' >> ~/.zshenv
